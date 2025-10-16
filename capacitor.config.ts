df6ce70900b29d16518ed0cbfc8326bc4493a0cf
reky0 <105706157+reky0@users.noreply.github.com>
2025-10-16 13:30:04 +0200

    refactor!: rename package ID from io.ionic.starter to com.reky0.animigo

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.reky0.animigo',
   appName: 'AniMigo',
   webDir: 'www'
 };
