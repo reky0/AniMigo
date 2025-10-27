e7703d2975337d459d5acdadb07c9b8b4369fc1d
reky0 <105706157+reky0@users.noreply.github.com>
2025-10-27 18:12:27 +0100

    build: update environment variable

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   anilistClientIdWeb: '31298', // Replace with your actual client ID from https://anilist.co/settings/developer
   anilistClientIdApp: '31655', // Replace with your actual client ID from https://anilist.co/settings/developer
-  anilistRedirectUriWeb: 'http://localhost:8100/auth/callback',
+  anilistRedirectUriWeb: 'http://animigo.netlify.app/auth/callback',
   anilistRedirectUriApp: 'animigo://auth/callback'
 };
 
